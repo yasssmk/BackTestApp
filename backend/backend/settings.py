fac45b42b1372f3475ac21b96280fb738b0facb1
yasssmk <ysamkawi@gmail.com>
2024-03-18 16:06:40 +0100

    setup database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "backendApp",
-        "USER": "postgres",
-        "PASSWORD": "Yass147896",
-        "HOST": "localhost",
-        "PORT": 5432
+        "NAME": env('DATABASE_NAME'),
+        "USER": env('DATABASE_USERNAME'),
+        "PASSWORD": env('DATABASE_PASSWORD'),
+        "HOST": env('DATABASE_HOSTNAME'),
+        "PORT": env('DATABASE_PORT')
     }
 }
 
