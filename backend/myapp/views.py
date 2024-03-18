e7ffebf465eea2fe4d63bd460474ea7ce4830017
yasssmk <ysamkawi@gmail.com>
2024-03-18 17:28:51 +0100

    Create user serializer

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+class Register(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
