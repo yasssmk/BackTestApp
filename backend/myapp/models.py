0af02808753c68abb7e077011b85096e7aa84da5
yasssmk <ysamkawi@gmail.com>
2024-03-18 16:48:09 +0100

    create user model

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
