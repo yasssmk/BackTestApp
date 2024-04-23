259ef8b504620b5f64a174d9c659ab4995ceca8c
yasssmk <ysamkawi@gmail.com>
2024-04-23 11:13:48 +0200

    fix header searchbar

--- frontend/src/layout/MainLayout/Header/SearchSection/index.js
+++ frontend/src/layout/MainLayout/Header/SearchSection/index.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { useState, useEffect, useContext, useRef } from 'react';
 
 // material-ui
-import { useTheme, styled } from '@mui/material/styles';
+import { useTheme, styled, useMediaQuery } from '@mui/material';
 import { Avatar, Box, ButtonBase, Card, Grid, InputAdornment, OutlinedInput, Popper, Popover, Typography } from '@mui/material';
 
 // third-party
@@ -13,7 +13,7 @@ import Transitions from '../../../../ui-component/extended/Transitions';
 import DashboardContext from "../../../../context/DashboardContext"
 
 // assets
-import { IconAdjustmentsHorizontal, IconSearch, IconX } from '@tabler/icons-react';
+import { IconSearch, IconX } from '@tabler/icons-react';
 import { shouldForwardProp } from '@mui/system';
 
 // styles
@@ -61,10 +61,12 @@ const SuggestionBox = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   borderRadius: theme.shape.borderRadius,
   boxShadow: theme.shadows[4],
-  padding: theme.spacing(1),
+  width: 434,
+  marginLeft: 16,
+  paddingLeft: 16,
+  paddingRight: 16,
   zIndex: theme.zIndex.modal + 1,
   position: 'absolute',
-  width: '100%',
   marginTop: theme.spacing(1),
   '& div': {
     cursor: 'pointer',
@@ -72,72 +74,131 @@ const SuggestionBox = styled(Box)(({ theme }) => ({
     '&:hover': {
       backgroundColor: theme.palette.action.hover
     }
+  },
+  [theme.breakpoints.down('lg')]: {
+    width: 250
+  },
+  [theme.breakpoints.down('md')]: {
+    width: '86%',
+    marginLeft: 4,
+    background: '#fff'
   }
 }));
 
-// ==============================|| SEARCH INPUT - MOBILE||============================== //
+//==============================|| SEARCH INPUT - MOBILE||============================== //
 
-const MobileSearch = ({ value, setValue, popupState }) => {
+const MobileSearch = ({
+  value,
+  setValue,
+  popupState,
+  showDropdown,
+  handleInputChange,
+  handleSelectOption,
+  searchClicked,
+  history,
+  recommendations,
+  isLoading
+}) => {
   const theme = useTheme();
 
+  const handleOptionClick = (option) => {
+    handleSelectOption(option);
+    setValue(option.Company_Name);
+  };
+
   return (
-    <OutlineInputStyle
-      id="input-search-header"
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      placeholder="Search"
-      startAdornment={
-        <InputAdornment position="start">
-          <IconSearch stroke={1.5} size="1rem" color={theme.palette.grey[500]} />
-        </InputAdornment>
-      }
-      endAdornment={
-        <InputAdornment position="end">
-          <ButtonBase sx={{ borderRadius: '12px' }}>
-            <HeaderAvatarStyle variant="rounded">
-              <IconAdjustmentsHorizontal stroke={1.5} size="1.3rem" />
-            </HeaderAvatarStyle>
-          </ButtonBase>
-          <Box sx={{ ml: 2 }}>
-            <ButtonBase sx={{ borderRadius: '12px' }}>
-              <Avatar
-                variant="rounded"
-                sx={{
-                  ...theme.typography.commonAvatar,
-                  ...theme.typography.mediumAvatar,
-                  background: theme.palette.orange.light,
-                  color: theme.palette.orange.dark,
-                  '&:hover': {
-                    background: theme.palette.orange.dark,
-                    color: theme.palette.orange.light
-                  }
-                }}
-                {...bindToggle(popupState)}
-              >
-                <IconX stroke={1.5} size="1.3rem" />
-              </Avatar>
+    <>
+      <OutlineInputStyle
+        id="input-search-header"
+        value={value}
+        onChange={handleInputChange}
+        placeholder="Search"
+        disabled={isLoading}
+        startAdornment={
+          <InputAdornment position="start">
+            <ButtonBase onClick={searchClicked}>
+            <Avatar
+            sx={{
+                    ...theme.typography.commonAvatar,
+                    ...theme.typography.mediumAvatar,
+                    background: theme.palette.background.paper
+                    
+            }}
+            >
+              <IconSearch stroke={1.5} size="1rem" color={theme.palette.grey[500]} />
+            </Avatar>
             </ButtonBase>
-          </Box>
-        </InputAdornment>
-      }
-      aria-describedby="search-helper-text"
-      inputProps={{ 'aria-label': 'weight' }}
-    />
+          </InputAdornment>
+        }
+        endAdornment={
+          <InputAdornment position="end">
+            <Box sx={{ ml: 2 }}>
+              <ButtonBase sx={{ borderRadius: '12px' }}>
+                <Avatar
+                  variant="rounded"
+                  sx={{
+                    ...theme.typography.commonAvatar,
+                    ...theme.typography.mediumAvatar,
+                    background: theme.palette.orange.light,
+                    color: theme.palette.orange.dark,
+                    '&:hover': {
+                      background: theme.palette.orange.dark,
+                      color: theme.palette.orange.light,
+                    },
+                  }}
+                  {...bindToggle(popupState)}
+                >
+                  <IconX stroke={1.5} size="1.3rem" />
+                </Avatar>
+              </ButtonBase>
+            </Box>
+          </InputAdornment>
+        }
+        aria-describedby="search-helper-text"
+        inputProps={{ 'aria-label': 'weight' }}
+      />
+      {showDropdown && (
+        <SuggestionBox>
+          {value === ''
+            ? history.map((option) => (
+                <div key={option.id} onClick={() => handleOptionClick(option)}>
+                  <Typography>
+                    {option.Company_Name} ({option.Symbol})
+                  </Typography>
+                </div>
+              ))
+            : recommendations.map((option) => (
+                <div key={option.id} onClick={() => handleOptionClick(option)}>
+                  <Typography>
+                    {option.Company_Name} ({option.Symbol})
+                  </Typography>
+                </div>
+              ))}
+        </SuggestionBox>
+      )}
+    </>
   );
 };
 
 MobileSearch.propTypes = {
   value: PropTypes.string,
   setValue: PropTypes.func,
-  popupState: PopupState
+  popupState: PopupState,
+  showDropdown: PropTypes.bool,
+  handleInputChange: PropTypes.func,
+  handleSelectOption: PropTypes.func,
+  history: PropTypes.array,
+  recommendations: PropTypes.array,
 };
 
 // ==============================|| SEARCH INPUT ||============================== //
 
 const SearchSection = () => {
   const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   const [value, setValue] = useState('');
-  const { handleChange, recommendations, runBacktest, setSelectedOption, selectedOption, addHistory, history, isLoading  } = useContext(DashboardContext);
+  const { handleChange, recommendations, runBacktest, setSelectedOption, selectedOption, addHistory, history, isLoading } =
+    useContext(DashboardContext);
   const [showDropdown, setShowDropdown] = useState(false);
   const [inputValue, setInputValue] = useState('');
   const searchBarRef = useRef(null);
@@ -166,106 +227,122 @@ const SearchSection = () => {
   const handleKeyDown = (event) => {
     if (event.key === 'Enter' && selectedOption) {
       setShowDropdown(false);
-      runBacktest(selectedOption.Symbol); 
+      runBacktest(selectedOption.Symbol);
     }
   };
-  
+
   const searchClicked = () => {
     if (selectedOption) {
       setShowDropdown(false);
-      runBacktest(selectedOption.Symbol); 
+      runBacktest(selectedOption.Symbol);
     }
-  }; 
-  
+  };
+
   const handleSelectOption = (option) => {
     setInputValue(option.Company_Name);
     setSelectedOption(option);
-    addHistory(option); 
+    addHistory(option);
     setShowDropdown(false);
   };
 
   return (
     <>
-      <Box sx={{ display: { xs: 'block', md: 'none' } }}>
-        <PopupState variant="popper" popupId="demo-popup-popper">
-          {(popupState) => (
-            <>
-              <Box sx={{ ml: 2 }}>
-                <ButtonBase sx={{ borderRadius: '12px' }}>
-                  <HeaderAvatarStyle variant="rounded" {...bindToggle(popupState)}>
-                    <IconSearch stroke={1.5} size="1.2rem" />
+      {!isMobile && (
+        <Box component="div" sx={{ display: { xs: 'none', md: 'block' } }} ref={searchBarRef}>
+          <OutlineInputStyle
+            id="input-search-header"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyUp={handleKeyDown}
+            placeholder="Search"
+            disabled={isLoading}
+            startAdornment={
+              <InputAdornment position="start">
+                <ButtonBase onClick={searchClicked}>
+                  <HeaderAvatarStyle variant="rounded">
+                    <IconSearch stroke={1.5} size="1rem" />
                   </HeaderAvatarStyle>
                 </ButtonBase>
-              </Box>
-              <PopperStyle {...bindPopper(popupState)} transition>
-                {({ TransitionProps }) => (
-                  <>
-                    <Transitions type="zoom" {...TransitionProps} sx={{ transformOrigin: 'center left' }}>
-                      <Card
-                        sx={{
-                          background: '#fff',
-                          [theme.breakpoints.down('sm')]: {
-                            border: 0,
-                            boxShadow: 'none'
-                          }
-                        }}
-                      >
-                        <Box sx={{ p: 2 }}>
-                          <Grid container alignItems="center" justifyContent="space-between">
-                            <Grid item xs>
-                              <MobileSearch value={value} setValue={setValue} popupState={popupState} />
-                            </Grid>
-                          </Grid>
-                        </Box>
-                      </Card>
-                    </Transitions>
-                  </>
-                )}
-              </PopperStyle>
-            </>
+              </InputAdornment>
+            }
+            aria-describedby="search-helper-text"
+            inputProps={{ 'aria-label': 'weight' }}
+          />
+
+          {showDropdown && (
+            <SuggestionBox>
+              {value === ''
+                ? history.map((option) => (
+                    <div key={option.id} onClick={() => handleSelectOption(option)}>
+                      <Typography>
+                        {option.Company_Name} ({option.Symbol})
+                      </Typography>
+                    </div>
+                  ))
+                : recommendations.map((option) => (
+                    <div key={option.id} onClick={() => handleSelectOption(option)}>
+                      <Typography>
+                        {option.Company_Name} ({option.Symbol})
+                      </Typography>
+                    </div>
+                  ))}
+            </SuggestionBox>
           )}
-        </PopupState>
-      </Box>
-      <Box component="div" sx={{ display: { xs: 'none', md: 'block' }}} ref={searchBarRef}>
-        <OutlineInputStyle
-          id="input-search-header"
-          value={inputValue}
-          onChange={handleInputChange}
-          onKeyUp={handleKeyDown}
-          placeholder="Search"
-          startAdornment={
-            <InputAdornment position="start">
-              <ButtonBase onClick={searchClicked} >
-                <HeaderAvatarStyle variant="rounded">
-                  <IconSearch stroke={1.5} size="1rem"/>
-                </HeaderAvatarStyle>
-              </ButtonBase>
-            </InputAdornment> 
-          }
-          aria-describedby="search-helper-text"
-          inputProps={{ 'aria-label': 'weight' }}
-        />
-      
-      {showDropdown && (
-        <SuggestionBox>
-          {value === ""
-            ? history.map((option) => (
-                <div key={option.id} onClick={() => handleSelectOption(option)}>
-                  <Typography>
-                    {option.Company_Name} ({option.Symbol})
-                  </Typography>
-                </div>
-              ))
-            : recommendations.map((option) => (
-                <div key={option.id} onClick={() => handleSelectOption(option)}>
-                  <Typography>
-                    {option.Company_Name} ({option.Symbol})
-                  </Typography>
-                </div>
-              ))}
-        </SuggestionBox>
+        </Box>
       )}
-      </Box>
+      {isMobile && (
+        <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+          <PopupState variant="popper" popupId="demo-popup-popper">
+            {(popupState) => (
+              <>
+                <Box sx={{ ml: 2 }}>
+                  <ButtonBase sx={{ borderRadius: '12px' }}>
+                    <HeaderAvatarStyle variant="rounded" {...bindToggle(popupState)}>
+                      <IconSearch stroke={1.5} size="1.2rem" />
+                    </HeaderAvatarStyle>
+                  </ButtonBase>
+                </Box>
+                <PopperStyle {...bindPopper(popupState)} transition>
+                  {({ TransitionProps }) => (
+                    <>
+                      <Transitions type="zoom" {...TransitionProps} sx={{ transformOrigin: 'center left' }}>
+                        <Card
+                          sx={{
+                            background: '#fff',
+                            [theme.breakpoints.down('sm')]: {
+                              border: 0,
+                              boxShadow: 'none',
+                            },
+                          }}
+                        >
+                          <Box sx={{ p: 2 }}>
+                            <Grid container alignItems="center" justifyContent="space-between" ref={searchBarRef}>
+                              <Grid item xs>
+                                <MobileSearch
+                                  value={value}
+                                  setValue={setValue}
+                                  popupState={popupState}
+                                  showDropdown={showDropdown}
+                                  handleInputChange={handleInputChange}
+                                  handleSelectOption={handleSelectOption}
+                                  searchClicked = {searchClicked}
+                                  history={history}
+                                  recommendations={recommendations}
+                                  isLoading={isLoading}
+                                />
+                              </Grid>
+                            </Grid>
+                          </Box>
+                        </Card>
+                      </Transitions>
+                    </>
+                  )}
+                </PopperStyle>
+              </>
+            )}
+          </PopupState>
+        </Box>
+      )}
     </>
   );
 };
