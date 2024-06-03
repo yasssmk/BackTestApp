7b925a6ffd67e47dfd7737dd4ff42b887117d17e
yasssmk <ysamkawi@gmail.com>
2024-06-03 15:15:21 +0200

    add launch shell script

---
+++ frontend/start_frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the working directory
+cd /home/ubuntu/backtestApp/src/frontend
+
+# Run the build process
+/usr/bin/npm run build
+
+# Serve the build directory
+/usr/bin/serve -s build -l 3000
